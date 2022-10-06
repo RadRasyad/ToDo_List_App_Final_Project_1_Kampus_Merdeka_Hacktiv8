6a3a8cd808f2e948d49adb683c53a29ac1cacd65
Syaifun Nadhif <88659346+SyaifunNadhif@users.noreply.github.com>
2022-10-07 06:55:33 +0700

    ui

--- app/src/main/java/com/hacktiv8/todolist/MainActivity.java
+++ app/src/main/java/com/hacktiv8/todolist/MainActivity.java
@@ -1,14 +1,34 @@
 package com.hacktiv8.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.hacktiv8.todolist.data.model.Note;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView noteRv;
+
+    List<Note> noteList;
+    NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteRv = findViewById(R.id.note_rv);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        noteRv.setLayoutManager(layoutManager);
+
+
+
+
     }
 }
